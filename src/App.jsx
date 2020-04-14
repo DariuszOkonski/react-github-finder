f391eb62caf241693f8ac5c2dc751b09e299272f
Dariusz Okonski <darek200180@gmail.com>
2020-04-14 12:36:43 +0100

    replace render to component Error at error route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navigation from './Navigation/Navigation';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
+import Error from './Error/Error';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
             <Route exact path='/single-user' render={() => <h1>Single User</h1>} />
             <Route exact path='/multiple-users' render={() => <h1>Multiple Users</h1>} />
             <Route exact path='/about' component={About} />
-            <Route render={() => <h1>ERROR</h1>} />
+            <Route component={Error} />
           </Switch>
 
 
