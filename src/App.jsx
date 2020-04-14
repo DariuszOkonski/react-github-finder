ef0c1d1cd55d68c0a03140db4ca0298a53db5531
Dariusz Okonski <darek200180@gmail.com>
2020-04-14 12:01:12 +0100

    prepare basic routes for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './index.css';
 import Navigation from './Navigation/Navigation';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navigation />
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path='/' render={() => <h1>Main Page</h1>} />
+            <Route exact path='/single-user' render={() => <h1>Single User</h1>} />
+            <Route exact path='/multiple-users' render={() => <h1>Multiple Users</h1>} />
+            <Route exact path='/about' render={() => <h1>About</h1>} />
+            <Route render={() => <h1>ERROR</h1>} />
+          </Switch>
+
+
+        </div>
 
 
       </div>
