732d825cc7dcc1784bfea2bf11d3a8d6c3879c88
Dariusz Okonski <darek200180@gmail.com>
2020-04-14 11:50:51 +0100

    add bootstrap Navbar to Navigation

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav>
-      <h2>Navigation</h2>
+
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">Github Finder</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/single-user">Single User</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/multiple-users">Multiple Users</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+
+          </ul>
+
+        </div>
+      </div>
     </nav>
+
+
+
+
+
+
+
+
+
   );
 }
 
