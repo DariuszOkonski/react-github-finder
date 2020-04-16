66685211c8d35c2a8508fd08da418a1496026c1f
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 10:06:07 +0100

    add UserAlert component

---
+++ src/SingleUser/UserAlert.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserAlert = () => {
+  return (
+    <div className="alert alert-warning text-center p-2 mt-2" role="alert">
+      <h2>Enter Username</h2>
+    </div>
+  );
+}
+
+export default UserAlert;
