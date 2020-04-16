ca89df24854ae19d494f966b1499d64b26965fb6
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 08:47:31 +0100

    add UserRepos

---
+++ src/SingleUser/UserRepos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserRepos = (props) => {
+  console.log(props);
+  return (
+    <div className="card my-3">
+      <h5 className="card-header">Latest Repos</h5>
+      <div className="card-body">
+
+        Single Repo
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default UserRepos;
